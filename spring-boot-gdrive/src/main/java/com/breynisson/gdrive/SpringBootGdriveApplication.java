4c310e82c8fa7c7684fd34e354317d491b067f6f
Reynisson, Brynjar <brynjar.reynisson@sabre.com>
2024-04-25 12:47:26 +0000

    Initial google drive integration, forgotten files

---
+++ spring-boot-gdrive/src/main/java/com/breynisson/gdrive/SpringBootGdriveApplication.java
@@ -0,0 +1,13 @@
+package com.breynisson.gdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGdriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGdriveApplication.class, args);
+	}
+
+}
